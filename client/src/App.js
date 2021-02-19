0be2ab74ca43fc471d4fcd574fc0a48cf4f1aadf
Dan <code@haddigan.email>
2021-02-19 10:13:52 -0500

    get basic upload functionality working in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("img_upload", e.target.img_upload.files[0]);
+    const result = await fetch("/api/upload", {
+      method: "post",
+      body: formData,
+    });
+    const coords = await result.json();
+    console.log(coords);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Scanifly</header>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div>
+            File: <input type="file" name="img_upload" />
+          </div>
+          <input type="submit" value="Upload" />
+        </form>
+      </section>
     </div>
   );
 }
