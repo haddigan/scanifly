35fc4e904094c45a5c3f0cdf5dc48882866db053
Dan <code@haddigan.email>
2021-02-19 09:29:30 -0500

    read GPS coords of uploaded file from exif

--- app/server.js
+++ app/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const formidable = require("formidable");
+const { readFile } = require("fs-extra");
+require("express-async-errors");
 
 const PORT = 4001;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.post("/api/upload", async (req, res, next) => {
+  const form = formidable();
+  try {
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      const filepath = files.img_upload.path;
+      const buffer = await readFile(filepath);
+      const parser = require("exif-parser").create(buffer);
+      const result = parser.parse();
+      const { GPSLatitude, GPSLongitude } = result.tags;
+      res.json([GPSLatitude, GPSLongitude]);
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 });
 
 app.listen(PORT, () => {
